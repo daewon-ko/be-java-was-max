04a8f5b8818a0f8dfe63431d4dd1f346a6520d4b
pinetree <eodnjs568@gmail.com>
2023-05-08 20:51:07 +0900

    feat "웹서버 1단계 index.html 구현"
    1. BufferedReader을 InputStreamReader을 이용하여 입력받음.
    - readLine 메서드를 통해서 Url을 통해 HTTP 메서드 생성하게끔 작성
    2. line.split을 통해서 /index.html을 parsing

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +23,18 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+
+            String[] splited = line.split(" ");
+            String path = splited[1];
+            System.out.println(path);
+            logger.debug(line);
+            while (!line.equals("")) {
+                line = br.readLine();
+                logger.debug(line);
+            }
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates"+path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
